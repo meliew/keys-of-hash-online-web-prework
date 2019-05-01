5370895f701249e8ac40b4e8fab3854e6c315a54
Melissa Williams <melissa@brandmeetscopy.com>
2019-05-01 00:16:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     empty_array = []
     self.each do |key, value|
-      argumenets.each do |arg|
+      arguments.each do |arg|
         empty_array << key if value == arg
       end
     end
